78f40e759944ba879fd453349efe263f7226875a
pai <69026987+ELPanaJose@users.noreply.github.com>
2021-05-08 10:41:25 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,22 @@
-const { Router } = require('express');
+require("dotenv").config();
+const { Router } = require("express");
 const router = Router();
-const accountSid = ""
-const authToken = ""
-const client = require('twilio')(accountSid, authToken);
+const accountSid = process.env.SID;
+const authToken = process.env.TOKEN;
+const twilioNumber = process.env.TNUMBER;
+const myNumber = process.env.NUMBER;
+const client = require("twilio")(accountSid, authToken);
 
-router.post('/message-sent',(req, res)=>{
-const { message, name } = req.body;
-client.messages
-  .create({
-     body: `name: ${name}, message: ${message}` ,
-     from: 'Your twilio number',
-     to: 'your number'
-   })
-  .then(message => console.log(message.sid));
-res.redirect('/success.html');});
+router.post("/message-sent", (req, res) => {
+  const { name, message } = req.body;
+  client.messages
+    .create({
+      body: `name: ${name}\n message: ${message}`,
+      from: twilioNumber,
+      to: myNumber,
+    })
+    .then(message => console.log(message.sid));
+  res.redirect("/success.html");
+});
 
 module.exports = router;
